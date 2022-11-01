a525ca2c7e0c77f4f7b4ce83a540e8f681df88f7
Damjan Stojcev <109067762+damjanstojcev4@users.noreply.github.com>
2022-11-01 20:04:15 +0100

    Update App.java

--- IWA_toUpperCase_toLowerCase/src/strings/App.java
+++ IWA_toUpperCase_toLowerCase/src/strings/App.java
@@ -6,7 +6,7 @@ public class App {
     public static void main(String[] args) {
         ToLowerUpperCase toLowerUpperCase = new ToLowerUpperCase();
 
-        System.out.println(toLowerUpperCase.setSentence("HELLO").toLowerCase(Locale.ROOT));
-        System.out.println(toLowerUpperCase.setSentence("hello").toUpperCase(Locale.ROOT));
+        System.out.println(toLowerUpperCase.setSentence("TO LOWER CASE").toLowerCase(Locale.ROOT));
+        System.out.println(toLowerUpperCase.setSentence("to upper case").toUpperCase(Locale.ROOT));
     }
 }
